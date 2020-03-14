f41dbfddfb14986e39f849e1655c03d5e9a66ac2
Graham Farrell <grahamfarrell1984@gmail.com>
2020-03-14 19:57:59 +0000

    Add an about page.

--- Tascanna/app/controllers/pages_controller.rb
+++ Tascanna/app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController # Pages controller is inherited from Application controller
   def home
   end
+
+  def about
+  end
 end
--- Tascanna/config/routes.rb
+++ Tascanna/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-    root 'pages#home' # Sets the root as the pages controller and the home action
+    root 'pages#home' # Sets the root as the pages controller and execute the home action
+
+    get 'about' => 'pages#about' # Go to the pages controller and execute the about action
 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

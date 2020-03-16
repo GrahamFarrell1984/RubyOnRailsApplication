ac27836422443ca049d610652423e4620e67d545
Graham Farrell <grahamfarrell1984@gmail.com>
2020-03-16 15:44:15 +0000

    Add edit and update actions and created edit view file.

--- Tascanna/app/controllers/tasks_controller.rb
+++ Tascanna/app/controllers/tasks_controller.rb
@@ -9,13 +9,26 @@ class TasksController < ApplicationController
         if @task.save
             flash[:notice] = "Task was successfully created"
             redirect_to task_path(@task)
-
         else
             render 'new'
         end
     end
 
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(tasks_params)
+            flash[:notice] = "Task was successfully updated"
+            redirect_to task_path(@task)
+        else
+            render 'edit'
+        end
+    end
+
     def show
+        @task = Task.find(params[:id])
+    end
+
+    def edit
         @task = Task.find(params[:id])
     end
 

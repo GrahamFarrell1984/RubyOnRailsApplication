fcdf5ad62be91d6edd3e087febfc8f9b3685b711
Graham Farrell <grahamfarrell1984@gmail.com>
2020-03-17 11:53:41 +0000

    Add test page and Bootstrap panels.

--- Tascanna/app/controllers/pages_controller.rb
+++ Tascanna/app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController # Pages controller is inherited from Application controller
-  def home
-  end
+    def home
+    end
 
-  def about
-  end
+    def about
+    end
+
+    def test
+    end
 end
--- Tascanna/config/routes.rb
+++ Tascanna/config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
 
     get 'about' => 'pages#about' # Go to the pages controller and execute the about action
 
+    get 'test' => 'pages#test' # Go to the pages controller and execute the test action
+
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

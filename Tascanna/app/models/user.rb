b4a3ceb96b571369438cf26b6968ffbb053ed3b8
Graham Farrell <grahamfarrell1984@gmail.com>
2020-03-24 00:44:09 +0000

    Add validation for password attribute.

--- Tascanna/app/models/user.rb
+++ Tascanna/app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable
 
     has_many :tasks, dependent: :destroy # If the user deletes their account then all associated tasks get deleted too
+
+    validate :password_complexity
+
+    def password_complexity
+        return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,25}$/
+
+        errors.add :password, 'Complexity requirement not met. Length should be 8-25 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    end
 end

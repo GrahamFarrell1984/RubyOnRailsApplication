0b071fe6ae31630f30f379e4da6d4c53a46009f2
Graham Farrell <grahamfarrell1984@gmail.com>
2020-04-01 01:02:08 +0100

    Update titles of more tests.

--- Tascanna/test/models/user_test.rb
+++ Tascanna/test/models/user_test.rb
@@ -32,21 +32,21 @@ class UserTest < ActiveSupport::TestCase
         assert(user.valid?, "Password is not valid") # Asserts that user object password was valid, otherwise print "Password is not valid" in the terminal
     end
 
-    test "A user with no email and no password can not be saved" do
+    test "A user with no email and no password cannot be saved" do
         user = User.new() # Creates a new User object with no email and no password and stores it in a variable called user
         assert_equal("", user.email, "Email is not empty") # Assert that email is empty, otherwise print "Email is not empty" in the terminal
         assert_nil(user.password, "Password is not nil") # Assert that password is nil, otherwise print "Password is not nil" in the terminal
         assert(!user.save, "User was saved") # Asserts that user object was not saved, otherwise print "User was saved" in the terminal
     end
 
-    test "A user with valid email and no password can not be saved" do
+    test "A user with valid email and no password cannot be saved" do
         user = User.new(email: "userOne@testEmail.com") # Creates a new User object with valid email and no password and stores it in a variable called user
         assert_equal("userOne@testEmail.com", user.email, "Email does not match") # Assert that email is equal to "userOne@testEmail.com", otherwise print "Email does not match" in the terminal
         assert_nil(user.password, "Password is not nil") # Assert that password is nil, otherwise print "Password is not nil" in the terminal
         assert(!user.save, "User was saved") # Asserts that user object was not saved, otherwise print "User was saved" in the terminal
     end
 
-    test "A user with no email and valid password can not be saved" do
+    test "A user with no email and valid password cannot be saved" do
         user = User.new(password: "Pa$$w0rd") # Creates a new User object with no email and valid password and stores it in a variable called user
         assert_equal("", user.email, "Email is not empty") # Assert that email is equal to "", otherwise print "Email is not empty" in the terminal
         assert_equal("Pa$$w0rd", user.password, "Password does not match") # Assert that password is equal to "Pa$$w0rd", otherwise print "Password does not match" in the terminal
